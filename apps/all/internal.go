e5cf9d3c7eeb2a10ba6dea3e88f490fd6c354f7f
solodba <945623397@qq.com>
2024-03-14 13:09:24 +0800

    添加程序启动功能

--- apps/all/internal.go
+++ apps/all/internal.go
@@ -1,1 +1,5 @@
 package all
+
+import (
+	_ "github.com/solodba/iwechat/apps/wechat/impl"
+)
--- protocol/wechat.go
+++ protocol/wechat.go
@@ -1,21 +1,34 @@
 package protocol
 
+import (
+	"context"
+
+	"github.com/solodba/iwechat/apps/wechat"
+	"github.com/solodba/mcube/apps"
+)
+
+var (
+	ctx = context.Background()
+)
+
 // Wechat服务结构体
 type WechatService struct {
+	svc wechat.Service
 }
 
 // Wechat服务结构体初始化
 func NewWechatService() *WechatService {
-	return &WechatService{}
+	return &WechatService{
+		svc: apps.GetInternalApp(wechat.AppName).(wechat.Service),
+	}
 }
 
 // Wechat服务启动方法
 func (w *WechatService) Start() error {
-	return nil
+	return w.svc.ChatTextBot(ctx)
 }
 
 // Wechat服务停止方法
-// Http服务停止方法
 func (s *WechatService) Stop() error {
 	return nil
 }
